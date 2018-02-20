bc52bbd72d44f56b71a8b376e0ca7d36b3c317eb
Pascha <atom.tomcat@gmail.com>
2018-02-20 20:38:26 +0300

    added functionality to buttons

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Pascha on 20.02.2018.
+//  Copyright © 2018 Pascha. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel:   String?
+}
